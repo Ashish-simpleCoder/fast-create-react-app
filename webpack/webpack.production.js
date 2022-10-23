48b98290546c667c8bf59d0cbbed66f481c20a0b
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-10-23 19:27:48 +0530

    [feat] generating manifest file for production build

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -1,6 +1,8 @@
 const { Configuration, ProgressPlugin } = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const {WebpackManifestPlugin} = require('webpack-manifest-plugin')
+
 
 
 /** @param {Configuration} config */
@@ -21,7 +23,28 @@ module.exports = (config) => {
             filename: 'static/css/[name].[contenthash:8].css',
             chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
         }),
-        new CssMinimizerPlugin()
+        new CssMinimizerPlugin(),
+
+
+        // generating manifest file in prouction mode
+        new WebpackManifestPlugin({
+            fileName: 'asset-manifest.json',
+            publicPath: '/build',
+            generate: (seed, files, entrypoints) => {
+                const manifestFiles = files.reduce((manifest, file) => {
+                    manifest[file.name] = file.path;
+                    return manifest;
+                }, seed);
+                const entrypointFiles = entrypoints.main.filter(
+                    fileName => !fileName.endsWith('.map')
+                );
+
+                return {
+                    files: manifestFiles,
+                    entrypoints: entrypointFiles,
+                }
+            },
+        }),
 
     ]
 
