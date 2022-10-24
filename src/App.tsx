0f239450093e656e3886d4782c384925da5250b0
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-10-24 10:46:07 +0530

    [build] added support for typescript compilation

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import "style/index.scss"
 import Car from 'asset/car.jpg'
 import Header from "@comp/Header"
 
-
-function App(){
+function App() {
     const [c, setC] = useState(0)
     useEffect(() => {
         console.log('awesome')
     }, [])
 
-    return(
+    return (
         <div>
             <Header />
             <h1>Fast CRA</h1>
             <h3>counter - {c}</h3>
-            <button onClick={() => setC(v => v+1)}>increment</button>
+            <button onClick={() => setC(v => v + 1)}>increment</button>
             <img src={Car} alt="" />
             {process.env.test}
         </div>
